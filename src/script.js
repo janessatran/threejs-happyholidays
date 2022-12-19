b8c3a574caefb4e494c2aff7f8bb90241fc2bca6
Janessa Tran <janessat@adobe.com>
2022-12-18 16:35:45 -0800

    update

--- src/script.js
+++ src/script.js
@@ -483,12 +483,12 @@ gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
 gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
-// scene.add(moonLight);
+scene.add(moonLight);
 
 // Door Light
 const doorLight = new THREE.PointLight("#ff7d46", 1, 7);
 doorLight.position.set(0, 2.2, 2.7);
-// house.add(doorLight);
+house.add(doorLight);
 
 /**
  * Sizes
